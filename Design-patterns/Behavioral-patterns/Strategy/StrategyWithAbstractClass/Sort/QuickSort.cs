1eb715f190ad1320b71d688e5707bb070888838f
Oscar Javier Jiménez <leojim06@gmail.com>
2022-03-24 00:38:57 -0500

    Implementación de patrón strategy con Clase Abstracta
    * Se agrega la clase abstracta con un método sobreescrito para identificar la clase hija
    * Se agrega un método abstracto para identificar la estrategia que se quiere implementar
    * Se crean las clases concretas que heredan de la clase abstracta
    * Se realiza la implementación concreta usando polimorfismo
    * Se crea la relación de agregación en la clase contexto SortedList y la clase abstracta
    * El cliente crea las instancias necesarias para ejecutar el patrón

---
+++ Design-patterns/Behavioral-patterns/Strategy/StrategyWithAbstractClass/Sort/QuickSort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StrategyWithAbstractClass.Sort
+{
+    public class QuickSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            list.Sort();
+        }
+    }
+}
---
+++ Design-patterns/Behavioral-patterns/Strategy/StrategyWithAbstractClass/Sort/SortStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace StrategyWithAbstractClass.Sort
+{
+    public abstract class SortStrategy
+    {
+        public override string ToString()
+        {
+            return this.GetType().Name;
+        }
+        public abstract void Sort(List<string> list);
+    }
+}
