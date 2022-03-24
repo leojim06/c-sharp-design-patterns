81f990a39ad75ba0383c56b580657f0e0df8752d
Oscar Javier Jiménez <leojim06@gmail.com>
2022-03-24 00:07:01 -0500

    Implementación de patrón strategy con Interface
    * Se agrega la interfaz que contiene la firma para la estrategia que se quiere implementar
    * Se crean las clases concretas que implementan la interfaz
    * Se realiza la implementación concreta usando polimorfismo
    * Se crea la relación de agregación en la clase contexto Navigator y la interfaz
    * El cliente crea las instancias necesarias para ejecutar el patrón

--- Design-patterns/Behavioral-patterns/Strategy/StrategyWithInterface/Program.cs
+++ Design-patterns/Behavioral-patterns/Strategy/StrategyWithInterface/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StrategyWithInterface.Route;
+using StrategyWithInterface.Route.Interfaces;
+using System;
 
 namespace StrategyWithInterface
 {
@@ -6,7 +8,29 @@ namespace StrategyWithInterface
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ejercicio de Patrón de comportamiento STRATEGY\n");
+
+            Navigator navigator = new Navigator();
+
+            Console.WriteLine("Seleccionando la primera estrategia: Viajar en transporte público");
+            IRouteStrategy strategy = new PublicTransportStrategy();
+            navigator.SetRouteStrategy(strategy);
+            string route = navigator.CalculateRoute("centro", "nor-oriente");
+            Console.WriteLine(route + "\n");
+
+            Console.WriteLine("Seleccionando la segunda estrategia: Caminar");
+            strategy = new WalkingStrategy();
+            navigator.SetRouteStrategy(strategy);
+            route = navigator.CalculateRoute("la entrada del bazar", "tienda de souvenir");
+            Console.WriteLine(route + "\n");
+
+            Console.WriteLine("Seleccionando la tercera estrategia: Ruta");
+            strategy = new RoadStrategy();
+            navigator.SetRouteStrategy(strategy);
+            route = navigator.CalculateRoute("calle 12", "calle 80");
+            Console.WriteLine(route + "\n");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Design-patterns/Behavioral-patterns/Strategy/StrategyWithInterface/Route/WalkingStrategy.cs
@@ -0,0 +1,12 @@
+﻿using StrategyWithInterface.Route.Interfaces;
+
+namespace StrategyWithInterface.Route
+{
+    public class WalkingStrategy : IRouteStrategy
+    {
+        public string BuildRoute(string pointA, string pointB)
+        {
+            return $"Camina desde {pointA} hasta {pointB}";
+        }
+    }
+}
